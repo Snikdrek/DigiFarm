c9f271d10c6ec40ddeb063d768673e7bae308920
RathijitAich <u2104014@student.cuet.ac.bd>
2026-01-17 19:11:00 +0600

    Fixed api issues. Env File created and not commited. Via gitignore

--- src/components/MarketPrices.js
+++ src/components/MarketPrices.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 
 /* ================= GEMINI REST CONFIG ================= */
-// ⚠️ DEMO ONLY — API KEY IS EXPOSED
-const GEMINI_API_KEY = "AIzaSyCeGLtyvzZ2EI9kh22tXYRrXmO8ATl7ebM";
+const GEMINI_API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
 
 const GEMINI_URL =
   `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`;
